901e4b39049eb6ac8cf66ce1bf8a9684da4b0584
Simon Beck <simon.beck@ateleris.ch>
2026-02-20 16:43:41 +0100

    setup fixes

--- e2eqss_defs/targets.cmake
+++ e2eqss_defs/targets.cmake
@@ -102,7 +102,7 @@ list(APPEND MISSION_GLOBAL_APPLIST cryptolib pqclean_cfs_wrapper libossl_cfs_wra
 SET(MISSION_CPUNAMES cpu1)
 
 SET(cpu1_PROCESSORID 1)
-SET(cpu1_APPLIST ci_lab to_lab)
+SET(cpu1_APPLIST ci_lab to_lab sch_lab)
 SET(cpu1_FILELIST cfe_es_startup.scr)
 SET(cpu1_SYSTEM i686-linux-gnu)
 
